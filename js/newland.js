3b1adbecf703a1df57223f02962af6abfe8e644f
Sergey Tarasenko <67330524+TatisWeb@users.noreply.github.com>
2024-08-06 04:38:05 +0400

    Add files via upload

--- js/newland.js
+++ js/newland.js
@@ -244,8 +244,16 @@ $(document).ready(function() {
              }
    }
  });
+   var owl = $(".anim-work");
+   owl.owlCarousel();
+   $('.js-owl-prev').click(function() {
+     owl.trigger('next.owl.carousel');
+   });
+   $('.js-owl-next').click(function() {
+   owl.trigger('prev.owl.carousel');
+   });
+   });
 
- });
  
  //слайдер кулон
 $(document).ready(function() {
@@ -682,45 +690,4 @@ $(document).ready(function() {
  })
 
 });
-
-$(document).ready(function() {
-
- // Declare Carousel jquery object
- var owl = $('.anim-work');
-
- // Carousel initialization
- 
- // add animate.css class(es) to the elements to be animated
- function setAnimation(_elem, _InOut) {
-  // Store all animationend event name in a string.
-  // cf animate.css documentation
-  var animationEndEvent = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-
-  _elem.each(function() {
-   var $elem = $(this);
-   var $animationType = 'animated ' + $elem.data('animation-' + _InOut);
-
-   $elem.addClass($animationType).one(animationEndEvent, function() {
-    $elem.removeClass($animationType); // remove animate.css Class at the end of the animations
-   });
-  });
- }
-
- // Fired before current slide change
- owl.on('change.owl.carousel', function(event) {
-  var $currentItem = $('.owl-item', owl).eq(event.item.index);
-  var $elemsToanim = $currentItem.find("[data-animation-out]");
-  setAnimation($elemsToanim, 'out');
- });
-
- // Fired after current slide has been changed
- owl.on('changed.owl.carousel', function(event) {
-
-  var $currentItem = $('.owl-item', owl).eq(event.item.index);
-  var $elemsToanim = $currentItem.find("[data-animation-in]");
-  setAnimation($elemsToanim, 'in');
- })
-
-});
-
 
